f951a5986e894f56c4639cf7630bba72b54545ba
Chaiyapon16091996 <chaiyapon2015@outlook.co.th>
2021-11-29 14:37:43 +0700

    Update menuControl.js

--- src/data/menuControl.js
+++ src/data/menuControl.js
@@ -50,11 +50,11 @@ export const menuItemsForSuperAdmin = [
     icon: "assessment",
     path: "/DataVolume",
   },
-  {
-    text: "รายการบังคับ",
-    icon: "playlist_add_check",
-    path: "/MandatoryItem",
-  },
+  // {
+  //   text: "รายการบังคับ",
+  //   icon: "playlist_add_check",
+  //   path: "/MandatoryItem",
+  // },
   // {
   //   text: "test",
   //   icon: "playlist_add_check",
