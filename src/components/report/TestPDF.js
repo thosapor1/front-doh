9331e9aa3964ddef3b1ccab9d4b0a255024c07c4
thosaporn <thosapor@hotmail.com>
2022-01-17 22:08:02 +0700

    gen pdf

--- src/components/report/TestPDF.js
+++ src/components/report/TestPDF.js
@@ -20,7 +20,7 @@ pdfMake.fonts = {
 export default function TestPDF() {
   // const [open, setOpen] = useState(false);
   // const handleClose = setOpen(false);
-  const url = "http://1d32-45-117-208-162.ap.ngrok.io/selectall-2";
+  const url = "http://1d32-45-117-208-162.ap.ngrok.io/selectall-3";
   let sendData = { date: "2022-01-01" };
 
   let body = [
@@ -77,11 +77,11 @@ export default function TestPDF() {
     });
   };
 
-  // Swal.fire({
-  //   title: "ขั้นตอนนี้อาจใช้เวลานาน",
-  //   allowOutsideClick: false,
-  //   didOpen: () => Swal.showLoading(),
-  // });
+  Swal.fire({
+    title: "ขั้นตอนนี้อาจใช้เวลานาน",
+    allowOutsideClick: false,
+    didOpen: () => Swal.showLoading(),
+  });
 
   axios
     .post(url, sendData, {
@@ -93,63 +93,49 @@ export default function TestPDF() {
       // console.log(res.headers);
       // setOpen(true);
       console.log(res.data);
-      let longString = res.data.slice(1, -1);
-      console.log("longString : ", longString);
-      let result = [];
-      longString
-        // .split("\n")
-        .split("},")
-        .map((item) => {
-          // JSON.parse(item);
-          // item.toString().replace("\\", "");
-          // let tranformItem = JSON.parse(item);
-          console.log(item);
-          result.push(item);
-          return result;
-        });
-      console.log("result :", result);
-      // JSON.parse(result);
-      // console.log("res :", result[1].readFlag);
-
-      const limit = 20000;
-      let remainData = Math.ceil((res.length - 1) / (limit - 1));
-      for (let round = 1; round <= remainData; round++) {
-        let index = 0;
-        if (round > 1) {
-          index = (round - 1) * limit + 1;
-        }
-        for (index; index < limit + 1; index++) {
-          if (round > 1) {
-          }
-          body.push([
-            !!res[index].transactionId ? res[index].transactionId : "-",
-            !!res[index].readFlag ? res[index].readFlag : "-",
-            !!res[index].match_timestamp
-              ? res[index].match_timestamp.split(" ")[0]
-              : "-",
-            !!res[index].match_timestamp
-              ? res[index].match_timestamp.split(" ")[1]
-              : "-",
-            `C${res[index].mf_lane_vehicleClass}`,
-            !!res[index].match_real_fee ? res[index].match_real_fee : "-",
-            !!res[index].match_real_fee ? res[index].match_real_fee : "-",
-            !!res[index].match_timestamp
-              ? res[index].match_timestamp.split(" ")[0]
-              : "-",
-            !!res[index].match_timestamp
-              ? res[index].match_timestamp.split(" ")[1]
-              : "-",
-            !!res[index].hasPayment ? res[index].hasPayment : "-",
-            !!res[index].readFlag ? res[index].readFlag : "-",
-          ]);
-        }
-        console.log("round : ", round);
-        console.log(body);
-        // pdfMake.createPdf(docDefinition).open({}, win);
-        await pdfGenDownload(docDefinition);
+      // const limit = 20000;
+      // let remainData = Math.ceil((res.length - 1) / (limit - 1));
+      // for (let round = 1; round <= remainData; round++) {
+      //   let index = 0;
+      //   if (round > 1) {
+      //     index = (round - 1) * limit + 1;
+      //   }
+      for (let index = 0; index < res.data.length - 1; index++) {
+        // if (round > 1) {
+        // }
+        body.push([
+          !!res.data[index].transactionId ? res.data[index].transactionId : "-",
+          !!res.data[index].readFlag ? res.data[index].readFlag : "-",
+          !!res.data[index].match_timestamp
+            ? res.data[index].match_timestamp.split(" ")[0]
+            : "-",
+          !!res.data[index].match_timestamp
+            ? res.data[index].match_timestamp.split(" ")[1]
+            : "-",
+          `C${res.data[index].mf_lane_vehicleClass}`,
+          !!res.data[index].match_real_fee
+            ? res.data[index].match_real_fee
+            : "-",
+          !!res.data[index].match_real_fee
+            ? res.data[index].match_real_fee
+            : "-",
+          !!res.data[index].match_timestamp
+            ? res.data[index].match_timestamp.split(" ")[0]
+            : "-",
+          !!res.data[index].match_timestamp
+            ? res.data[index].match_timestamp.split(" ")[1]
+            : "-",
+          !!res.data[index].hasPayment ? res.data[index].hasPayment : "-",
+          !!res.data[index].readFlag ? res.data[index].readFlag : "-",
+        ]);
       }
+      // console.log("round : ", round);
+      console.log(body);
+      // pdfMake.createPdf(docDefinition).open({}, win);
+      await pdfGenDownload(docDefinition);
+      // }
+      Swal.close();
     });
-  // Swal.close();
 
   // axios
   //   .post(url, sendData, {
@@ -161,8 +147,8 @@ export default function TestPDF() {
   //     },
   //   })
   //   .then((res) => {
-  //     // console.log(res.headers);
-  //     // console.log(typeof res.data);
+  // console.log(res.headers);
+  // console.log(typeof res.data);
   //     let response = res.data ? JSON.stringify(res.data) : [];
   //     let response1 = res.data ? JSON.parse(response) : [];
   //     console.log(response1);
@@ -176,8 +162,8 @@ export default function TestPDF() {
   //     let round = 0;
   //     for (round; round < response.length - 1; round++) {
   //       if (index < limit) {
-  //         // console.log("first if");
-  //         // console.log(headerFlag);
+  // console.log("first if");
+  // console.log(headerFlag);
   //         if (headerFlag === 0) {
   //           body.push(
   //             [
@@ -319,19 +305,19 @@ export default function TestPDF() {
                 bold: true,
                 margin: [20, 5, 0, 0],
               },
-              // {
-              //   absolutePosition: { x: 30, y: 20 },
-              //   canvas: [
-              //     {
-              //       type: "line",
-              //       x1: 20,
-              //       y1: 2,
-              //       x2: 700,
-              //       y2: 2,
-              //       lineWidth: 1,
-              //     },
-              //   ],
-              // },
+              {
+                //   absolutePosition: { x: 30, y: 20 },
+                canvas: [
+                  {
+                    type: "line",
+                    x1: 20,
+                    y1: 2,
+                    x2: 700,
+                    y2: 2,
+                    lineWidth: 1,
+                  },
+                ],
+              },
               {
                 text: "กองทางหลวงพิเศษระหว่างเมือง กรมทางหลวง",
                 fontSize: 14,
@@ -383,9 +369,5 @@ export default function TestPDF() {
   // pdfMake.createPdf(docDefinition).download("รายงานประจำวัน.pdf");
   // pdfMake.createPdf(docDefinition).open({}, win);
 
-  return (
-    <div>
-      {/* <ModalProgress open={open} onClose={handleClose} /> */}
-    </div>
-  );
+  return <div>{/* <ModalProgress open={open} onClose={handleClose} /> */}</div>;
 }
